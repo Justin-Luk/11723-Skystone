a22fb937ce4f63a07997b89a50506274c8fd9bd1
Justin <42159955+Justin-Luk@users.noreply.github.com>
2019-12-13 16:32:14 -0500

    Add files via upload

--- testst.java
+++ testst.java
@@ -1,25 +1,27 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class testst extends OpMode {
-    private DcMotor RF,RB,LF,LB,FI;
+    private DcMotor RF,RB,LF,LB,FI,Cranemotor;
     private Servo Arm = null;
-    private Servo Arm2 = null;
+    private CRServo Crane1 = null;
+
     @Override
     public void init() {
+
         RF = hardwareMap.dcMotor.get("RF"); //gets RFM on hardware map
         RB = hardwareMap.dcMotor.get("RB"); //gets RBM on hardware map
         LF = hardwareMap.dcMotor.get("LF"); //gets LFM on hardware map
         LB = hardwareMap.dcMotor.get("LB"); //gets LBM on hardware map
         FI = hardwareMap.dcMotor.get("FI"); //gets Front Intake on hardware map
-       // Arm = hardwareMap.dcMotor.get("ARM");
+        Cranemotor = hardwareMap.dcMotor.get("Cranemotor");
         Arm = hardwareMap.servo.get("ARM");
-        Arm2 = hardwareMap.servo.get("ARM2");
-
+        Crane1  = hardwareMap.crservo.get("Crane1");
 
         RB.setDirection(DcMotor.Direction.REVERSE); //sets both left side motors on reverse
         RF.setDirection(DcMotor.Direction.REVERSE);
@@ -37,31 +39,48 @@ public class testst extends OpMode {
 
     @Override
     public void loop() {
-        RF.setPower(0); //establishes basic tank drive controls
-        RB.setPower(0);
-        LF.setPower(0);
-        LB.setPower(0);
+//        RF.setPower(0); //establishes basic tank drive controls
+//        RB.setPower(0);
+//        LF.setPower(0);
+//        LB.setPower(0);
 
         RF.setPower(-(gamepad1.right_stick_y)); //establishes basic tank drive controls
         RB.setPower(-(gamepad1.right_stick_y));
         LF.setPower(-(gamepad1.left_stick_y));
         LB.setPower(-(gamepad1.left_stick_y));
 
+        while (gamepad2.right_trigger > 0) {
+            Cranemotor.setPower(0.5);
+        }
+        while (gamepad2.left_trigger > 0) {
+            Cranemotor.setPower(-0.5);
+        }
+        if (gamepad2.right_bumper){
+            Crane1.setPower(0.5);
+        }
+        if (gamepad2.left_bumper) {
+            Crane1.setPower(-0.5);
+        }
+
         while (gamepad1.right_stick_x > 0) {
             RF.setPower(1);
-            LB.setPower(1);
+            LF.setPower(1);
+
         }
         while (gamepad1.right_stick_x < 0) {
             RF.setPower(-1);
-            LB.setPower(-1);
-        }
-        while (gamepad1.left_stick_x < 0) {
-            LF.setPower(1);
-            RB.setPower(1);
+            LF.setPower(-1);
+
         }
         while (gamepad1.left_stick_x > 0) {
-            LF.setPower(1);
+            LB.setPower(1);
             RB.setPower(1);
+
+        }
+        while (gamepad1.left_stick_x < 0) {
+            LB.setPower(-1);
+            RB.setPower(-1);
+
         }
      //   Arm.setPower(gamepad2.left_stick_y);
 
@@ -106,12 +125,6 @@ public class testst extends OpMode {
         }
         if (gamepad2.left_bumper) {
             Arm.setPosition(.6);
-        }
-        if (gamepad2.left_stick_button){
-            Arm2.setPosition(.6);
-        }
-        if (gamepad2.right_stick_button){
-            Arm2.setPosition(1);
         }
 
 
