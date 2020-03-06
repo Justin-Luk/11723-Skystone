050ec2b06b051e1d69ea163fd0f443d6d2526544
Justin <42159955+Justin-Luk@users.noreply.github.com>
2020-03-06 16:59:46 -0500

    Add files via upload

--- testst.java
+++ testst.java
@@ -11,13 +11,14 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp
+@TeleOp (name="our actual teleop")
 public class testst extends OpMode {
     private DcMotor RF,RB,LF,LB,FI,Cranemotor;
     private CRServo Crane1 = null;
     private Servo Arm2;
-   // private Servo CapServo;
-    int pulse = 1680;
+    double motorpowerleft =Math.pow(gamepad1.left_stick_y, 2);
+    double motorpowerright =Math.pow(gamepad1.right_stick_y,2);
+    private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
 
 
@@ -57,6 +58,7 @@ public class testst extends OpMode {
 
     @Override
     public void loop() {
+
 //        RF.setPower(0); //establishes basic tank drive controls
 //        RB.setPower(0);
 //        LF.setPower(0);
@@ -82,46 +84,53 @@ public class testst extends OpMode {
 //        LB.setPower(plb);
 //        RF.setPower(prf);
 //        RB.setPower(prb);
+        telemetry.addData("timer", timer);
 
-        telemetry.addLine("Arcade Drive");
-        double px = gamepad1.left_stick_x;
-        if (Math.abs(px) < 0.05) px = 0;
-        double py = -gamepad1.left_stick_y;
-        if (Math.abs(py) < 0.05) py = 0;
-        double pa = -(gamepad1.right_stick_x*(.70));
-        if (Math.abs(pa) < 0.05) pa = 0;
-        double plf = -px + py - pa;
-        double plb = px + py + -pa;
-        double prf = -px + py + pa;
-        double prb = px + py + pa;
-        double max = Math.max(1.0, Math.abs(plf));
-        max = Math.max(max, Math.abs(plb));
-        max = Math.max(max, Math.abs(prf));
-        max = Math.max(max, Math.abs(prb));
-        plf /= max;
-        plb /= max;
-        prf /= max;
-        prb /= max;
-        LF.setPower(plf);
-        LB.setPower(plb);
-        RF.setPower(prf);
-        RB.setPower(prb);
+        if (gamepad1.b && gamepad1.a) {
+            LF.setPower(motorpowerleft);
+            LB.setPower(motorpowerleft);
+            RF.setPower(motorpowerright);
+            RB.setPower(motorpowerright);
+        } else {
+            telemetry.addLine("Arcade Drive");
+            double px = gamepad1.left_stick_x;
+            if (Math.abs(px) < 0.05) px = 0;
+            double py = -gamepad1.left_stick_y;
+            if (Math.abs(py) < 0.05) py = 0;
+            double pa = -(gamepad1.right_stick_x*(.70));
+            if (Math.abs(pa) < 0.05) pa = 0;
+            double plf = -px + py - pa;
+            double plb = px + py + -pa;
+            double prf = -px + py + pa;
+            double prb = px + py + pa;
+            double max = Math.max(1.0, Math.abs(plf));
+            max = Math.max(max, Math.abs(plb));
+            max = Math.max(max, Math.abs(prf));
+            max = Math.max(max, Math.abs(prb));
+            plf /= max;
+            plb /= max;
+            prf /= max;
+            prb /= max;
+            LF.setPower(plf);
+            LB.setPower(plb);
+            RF.setPower(prf);
+            RB.setPower(prb);
 //        RF.setPower(-(gamepad1.right_stick_y)); //establishes basic tank drive controls
 //        RB.setPower(-(gamepad1.right_stick_y));
 //        LF.setPower(-(gamepad1.left_stick_y));
 //        LB.setPower(-(gamepad1.left_stick_y));
-    //    Arm2.setPower(gamepad2.right_stick_y);
+            //    Arm2.setPower(gamepad2.right_stick_y);
 
 //        if(gamepad2.dpad_up) {
 //            Arm2.set;
 //        }
-        if  (gamepad2.dpad_right){
-            Arm2.setPosition(0);
-        }
-        if (gamepad2.dpad_left){
-            Arm2.setPosition(.4);
-        }
-        Crane1.setPower(gamepad2.left_stick_y);
+            if  (gamepad2.dpad_right){
+                Arm2.setPosition(0);
+            }
+            if (gamepad2.dpad_left){
+                Arm2.setPosition(.4);
+            }
+            Crane1.setPower(gamepad2.left_stick_y);
 //            Cranemotor.setPower(0);
 //        }
 
@@ -134,102 +143,108 @@ public class testst extends OpMode {
 //            CapServo.setPosition(.8);
 //        }
 
-        while (gamepad1.right_trigger > 0)    {
-            LF.setPower(1);
-            LB.setPower(-1);
-            RF.setPower(-1);
-            RB.setPower(1);
-        }
-
-        while (gamepad1.left_trigger > 0)       {
-            LF.setPower(-1);
-            LB.setPower(1);
-            RF.setPower(1);
-            RB.setPower(-1);
-        }
-
-        while (gamepad1.right_bumper)    {
-            LF.setPower(0.5);
-            LB.setPower(-0.5);
-            RF.setPower(-0.5);
-            RB.setPower(0.5);
-        }
-
-        while (gamepad1.left_bumper)       {
-            LF.setPower(-0.5);
-            LB.setPower(0.5);
-            RF.setPower(0.5);
-            RB.setPower(-0.5);
-        }
-
-        if (gamepad1.x)       {
-            LF.setPower(1);
-            LB.setPower(1);
-            RF.setPower(1);
-            RB.setPower(1);
-        }
-
-
-        if (gamepad1.y) {
-
-            LF.setPower(1);
-            LB.setPower(1);
-            RF.setPower(1);
-            RB.setPower(1);
-            ElapsedTime elapsedTime = new ElapsedTime();
-            FI.setPower(-.5);
-            elapsedTime.reset();
-
-            while (elapsedTime.seconds() < 1){
-
+            while (gamepad1.right_trigger > 0)    {
+                LF.setPower(1);
+                LB.setPower(-1);
+                RF.setPower(-1);
+                RB.setPower(1);
             }
-        }
-        if (gamepad2.b) {
-            ElapsedTime elapsedTime = new ElapsedTime();
-            FI.setPower(.5);
-            elapsedTime.reset();
 
-            while (elapsedTime.seconds() < 2){
-
+            while (gamepad1.left_trigger > 0)       {
+                LF.setPower(-1);
+                LB.setPower(1);
+                RF.setPower(1);
+                RB.setPower(-1);
             }
-            FI.setPower(0);
-        }
 
-        if (gamepad2.right_trigger > .3) {
-            Cranemotor.setPower(1);
-        } else {
-            Cranemotor.setPower(0);
-        }
-        if (gamepad2.left_trigger > .3) {
-            Cranemotor.setPower(-1);
-        } else {
-            Cranemotor.setPower(0);
-        }
-
-        if (gamepad2.a) {
-            ElapsedTime elapsedTime = new ElapsedTime();
-            FI.setPower(-.5);
-            elapsedTime.reset();
-
-            while (elapsedTime.seconds() < 2){
-
+            while (gamepad1.right_bumper)    {
+                LF.setPower(0.5);
+                LB.setPower(-0.5);
+                RF.setPower(-0.5);
+                RB.setPower(0.5);
             }
-            FI.setPower(0);
-        }
-       FI.setPower(gamepad2.right_stick_x);
 
-        if (gamepad2.x) {
+            while (gamepad1.left_bumper)       {
+                LF.setPower(-0.5);
+                LB.setPower(0.5);
+                RF.setPower(0.5);
+                RB.setPower(-0.5);
+            }
 
-            {
+            if (gamepad1.x)       {
+                LF.setPower(1);
+                LB.setPower(1);
+                RF.setPower(1);
+                RB.setPower(1);
+            }
+
+
+            if (gamepad1.y) {
+
+                LF.setPower(1);
+                LB.setPower(1);
+                RF.setPower(1);
+                RB.setPower(1);
                 ElapsedTime elapsedTime = new ElapsedTime();
-                Crane1.setPower(-1);
                 FI.setPower(-.5);
                 elapsedTime.reset();
 
-                while (elapsedTime.seconds() < 2) {
+                while (elapsedTime.seconds() < 1){
 
                 }
             }
+            if (gamepad2.b) {
+                ElapsedTime elapsedTime = new ElapsedTime();
+                FI.setPower(.5);
+                elapsedTime.reset();
+
+                while (elapsedTime.seconds() < 2){
+
+                }
+                FI.setPower(0);
+            }
+
+            if (gamepad2.right_trigger > .3) {
+                Cranemotor.setPower(1);
+            } else {
+                Cranemotor.setPower(0);
+            }
+            if (gamepad2.left_trigger > .3) {
+                Cranemotor.setPower(-1);
+            } else {
+                Cranemotor.setPower(0);
+            }
+
+            Cranemotor.setPower(gamepad2.right_trigger);
+            Cranemotor.setPower(-gamepad2.left_trigger);
+
+            if (gamepad2.a) {
+                ElapsedTime elapsedTime = new ElapsedTime();
+                FI.setPower(-.5);
+                elapsedTime.reset();
+
+                while (elapsedTime.seconds() < 2){
+
+                }
+                FI.setPower(0);
+            }
+            FI.setPower(gamepad2.right_stick_x);
+
+            if (gamepad2.x) {
+
+                {
+                    ElapsedTime elapsedTime = new ElapsedTime();
+                    Crane1.setPower(-1);
+                    FI.setPower(-.5);
+                    elapsedTime.reset();
+
+                    while (elapsedTime.seconds() < 2) {
+
+                    }
+                }
+            }
         }
+
+
 
     }}
